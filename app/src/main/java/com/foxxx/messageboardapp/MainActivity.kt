535f5007560e59103f958493600141edb6d0396b
Foxxx48 <Foxxdim@mail.ru>
2023-08-14 16:53:53 +0300

    Firebase added Authorisation with Google Account

--- app/src/main/java/com/foxxx/messageboardapp/MainActivity.kt
+++ app/src/main/java/com/foxxx/messageboardapp/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.foxxx.messageboardapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.foxxx.messageboardapp.databinding.ActivityMainBinding
+import com.foxxx.messageboardapp.dialogs.DialogConst
+import com.foxxx.messageboardapp.dialogs.DialogHelper
+import com.foxxx.messageboardapp.dialogs.GoogleAccConst
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    private lateinit var tvAccount: TextView
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val dialogHelper = DialogHelper(this)
+
+    val myAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +35,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         init()
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == GoogleAccConst.GOOGLE_SIGN_IN_REQUEST_CODE){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null){
+                    dialogHelper.accHelper.signInFirebaseWithGoogle(account.idToken!!)
+                }
+            }catch (e: ApiException){
+                Log.d("MyLog", "API error: ${e.message}")
+            }
+            /*
+                        Log.d("MyLog", "Sign in result")
+            */
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
     private fun init() {
         with(binding) {
             val toggle =
@@ -36,47 +69,64 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             toggle.syncState()
 
             navView.setNavigationItemSelectedListener(this@MainActivity)
+
+            tvAccount = binding.navView.getHeaderView(0).findViewById(R.id.tvAccountEmail)
+
         }
     }
 
-        override fun onNavigationItemSelected(item: MenuItem): Boolean {
-            when (item.itemId) {
-                R.id.ad_my_ads -> {
-                    Log.d("Item", "ad_my_ads")
-                    Toast.makeText(this, "Presed ad_my_ads", Toast.LENGTH_LONG).show()
-                }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.ad_my_ads -> {
 
-                R.id.ad_car -> {
-                    Log.d("Item", "ad_car")
-                    Toast.makeText(this, "Presed ad_car", Toast.LENGTH_LONG).show()
-                }
-
-                R.id.ad_pc -> {
-                    Toast.makeText(this, "Presed ad_pc", Toast.LENGTH_LONG).show()
-                }
-
-                R.id.ad_dm -> {
-                    Toast.makeText(this, "Presed ad_dm", Toast.LENGTH_LONG).show()
-                }
-
-                R.id.ad_smart -> {
-                    Toast.makeText(this, "Presed ad_smart", Toast.LENGTH_LONG).show()
-                }
-
-                R.id.sign_in -> {
-//                dialogHelper.createSignDialog(DialogConst.SIGN_IN_STATE)
-                }
-
-                R.id.sign_out -> {
-//                uiUpdate(null)
-//                myAuth.signOut()
-                }
-
-                R.id.sign_up -> {
-//                dialogHelper.createSignDialog(DialogConst.SIGN_UP_STATE)
-                }
+                Toast.makeText(this, "Presed ad_my_ads", Toast.LENGTH_LONG).show()
             }
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-            return true
+
+            R.id.ad_car -> {
+
+                Toast.makeText(this, "Presed ad_car", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.ad_pc -> {
+                Toast.makeText(this, "Presed ad_pc", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.ad_dm -> {
+                Toast.makeText(this, "Presed ad_dm", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.ad_smart -> {
+                Toast.makeText(this, "Presed ad_smart", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.sign_in -> {
+                dialogHelper.createSignInDialog(DialogConst.SIGN_IN_STATE)
+            }
+
+            R.id.sign_out -> {
+                uiUpdate(null)
+                myAuth.signOut()
+            }
+
+            R.id.sign_up -> {
+                dialogHelper.createSignInDialog(DialogConst.SIGN_UP_STATE)
+            }
         }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun uiUpdate(user: FirebaseUser?) {
+
+        tvAccount.text = if (user == null) {
+            resources.getString(R.string.not_reg)
+        } else {
+            user.email
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        uiUpdate(myAuth.currentUser)
+    }
 }
---
+++ app/src/main/java/com/foxxx/messageboardapp/dialogs/DialogHelper.kt
@@ -0,0 +1,86 @@
+package com.foxxx.messageboardapp.dialogs
+
+import android.app.AlertDialog
+import android.view.View
+import android.widget.Toast
+import com.foxxx.messageboardapp.MainActivity
+import com.foxxx.messageboardapp.R
+import com.foxxx.messageboardapp.accounthelper.AccountHelper
+import com.foxxx.messageboardapp.databinding.SignDialogBinding
+
+class DialogHelper(private val activity: MainActivity) {
+
+    val accHelper = AccountHelper(activity)
+
+    fun createSignInDialog(index: Int) {
+        val builder = AlertDialog.Builder(activity)
+        val rootDialogElement = SignDialogBinding.inflate(activity.layoutInflater)
+        builder.setView(rootDialogElement.root)
+
+        setDialogState(index, rootDialogElement)
+
+        val dialog = builder.create()
+
+        rootDialogElement.btnSignUpIn.setOnClickListener {
+            setOnClickSingUpIn(index, rootDialogElement, dialog)
+        }
+        rootDialogElement.btnForgotPassword.setOnClickListener {
+            setOnClickResetPassword(rootDialogElement, dialog)
+        }
+        rootDialogElement.btnGoogleSignIn.setOnClickListener {
+            accHelper.signInWithGoogle()
+        }
+
+        dialog.show()
+    }
+
+    private fun setOnClickSingUpIn(
+        index: Int,
+        rootDialogElement: SignDialogBinding,
+        dialog: AlertDialog?
+    ) {
+        dialog?.dismiss()
+        if (index == DialogConst.SIGN_UP_STATE) {
+            accHelper.signUpWithEmail(
+                rootDialogElement.edSignEmail.text.toString(),
+                rootDialogElement.edSignPassword.text.toString()
+            )
+        } else {
+            accHelper.signInWithEmail(
+                rootDialogElement.edSignEmail.text.toString(),
+                rootDialogElement.edSignPassword.text.toString()
+            )
+        }
+    }
+
+    private fun setOnClickResetPassword(
+        rootDialogElement: SignDialogBinding,
+        dialog: AlertDialog?
+    ) {
+
+        if (rootDialogElement.edSignEmail.text.isNotEmpty()) {
+            activity.myAuth.sendPasswordResetEmail(rootDialogElement.edSignEmail.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(activity, R.string.email_reset_password_send, Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            dialog?.dismiss()
+        } else {
+            rootDialogElement.tvDialogMessage.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setDialogState(index: Int, rootDialogElement: SignDialogBinding) {
+        if (index == DialogConst.SIGN_UP_STATE) {
+            rootDialogElement.tvSignTitle.text = activity.resources.getString(R.string.sign_up)
+            rootDialogElement.btnSignUpIn.text = activity.resources.getString(R.string.sign_up_action)
+
+        } else {
+            rootDialogElement.tvSignTitle.text = activity.resources.getString(R.string.sign_in)
+            rootDialogElement.btnSignUpIn.text = activity.resources.getString(R.string.sign_in_action)
+            rootDialogElement.btnForgotPassword.visibility = View.VISIBLE
+        }
+    }
+}
